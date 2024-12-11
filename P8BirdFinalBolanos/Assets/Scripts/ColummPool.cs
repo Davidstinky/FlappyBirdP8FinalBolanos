7d37b2c79fe2d9b518ecb321e4bede70a2f16f11
Davidstinky <751474@student.dusd.net>
2024-12-11 15:12:13 -0800

    finished my project
    i finished my project and now i just need to customize the sprites

--- P8BirdFinalBolanos/Assets/Scripts/ColummPool.cs
+++ P8BirdFinalBolanos/Assets/Scripts/ColummPool.cs
@@ -8,10 +8,14 @@ public class ColummPool : MonoBehaviour
     public int colummPoolSize = 5;
     public GameObject colummPrefab;
     public float spawnRate = 4f;
+    public float colummMin = -1f;
+    public float colummMax = 3.5f;
 
     private GameObject[] columms;
     private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
     private float timeSinceLastSpawned;
+    private float spawnXPosition = 10f;
+    private int currentColumm = 0;
 
 
     // Start is called before the first frame update
@@ -27,6 +31,18 @@ public class ColummPool : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.gameOver == false && timeSinceLastSpawned > spawnRate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(colummMin, colummMax);
+            columms [currentColumm].transform.position = new Vector2 (spawnXPosition, spawnYPosition);
+            currentColumm++;   
+            if (currentColumm >= colummPoolSize)
+            {
+                currentColumm = 0;
+            }
+        }
     }
 }
